4f84c05bc9a2c14f83d7c4f23d7768e5e3a94b19
Bart van Laatum <hbpvanlaatum123@gmail.com>
2019-01-09 11:09:32 +0100

    'notebook'

--- Bart/test_branch.py
+++ Bart/test_branch.py
@@ -1,3 +1,11 @@
-print("this is a side branch")
-def aanpassingtofile():
-	return
+import numpy as np
+import pandas as pd
+
+# return pandas dataframe given csv file
+def load_data(filename):
+	return pd.read_csv(filename, delimiter=';')
+
+
+if __name__ == "__main__":
+	alldata = load_data("VragenlijstdeelnemersDamtotDamloop_bewerkt_anoniem.csv")
+	print(alldata.head())
