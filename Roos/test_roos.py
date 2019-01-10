b5e0acb0accc8a992abd57ae5cb292271c7a5e74
Roos Riemersma <roos.riemersma@hotmail.com>
2019-01-10 10:49:20 +0100

    code verwijderen users

--- Roos/test_roos.py
+++ Roos/test_roos.py
@@ -1,1 +1,11 @@
-#hallo, zou chill zijn als dit werkt kusjes
+import pandas as pd
+import numpy as np
+
+# verwijder users die groot deel van de data niet hebben ingevuld
+data = pd.read_csv("relevantQs.csv", delimiter=',')
+data = data.replace(" ", np.nan)
+
+# lijst van indexen voor rijen waarbij meer dan 70 vragen niet zijn ingevuld
+nan = [index for index, row in data.iterrows() if row.isnull().sum() > 70]
+
+data = data.drop(data.index[nan])
