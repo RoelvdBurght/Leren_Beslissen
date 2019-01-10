00b7443849cc370dbefcace61cdc71d5a0567709
Roos Riemersma <roos.riemersma@hotmail.com>
2019-01-10 15:00:16 +0100

    drop alt columns

--- Roos/cleaning_data.py
+++ Roos/cleaning_data.py
@@ -11,12 +11,9 @@ nan = [index for index, row in data.iterrows() if row.isnull().sum() > 70]
 
 data = data.drop(data.index[nan])
 
-# NaN naar 0 en string naar 1 bij alle _alt vragen
+####################
+
+# drop _alt columns
 alt_columns = [column for column in data.columns.values if column[-3:] == 'alt']
 
-data[alt_columns] = data[alt_columns].replace(np.nan, 0)
-
-for key, value in data[alt_columns].iteritems():
-    for item in value:
-        if item != 0:
-            data[alt_columns] = data[alt_columns].replace(item, 1)  
+data = data.drop(alt_columns, axis=1)
