1fec5a7647a454d2d52bd1f6a56031adf1392a53
Roos Riemersma <roos.riemersma@hotmail.com>
2019-01-11 13:45:10 +0100

    new plots elbow

--- Roos/elbow_method.py
+++ Roos/elbow_method.py
@@ -8,19 +8,20 @@ import matplotlib.pyplot as plt
 
 # find optimal value of k for k-means
 data = pd.read_csv("../Data/one_hot_data.csv", delimiter=',')
-data = data.drop(['Q47_Man', 'Q47_Vrouw', 'Q48', 'Q49', 'Q50'], axis=1)
+#data = data.drop(['Q47_Man', 'Q47_Vrouw', 'Q48', 'Q49', 'Q50'], axis=1)
 
 X = data.values
 
-distortions = []
+Sum_of_squared_distances = []
 K = range(1,15)
 for k in K:
-    kmeanModel = KMeans(n_clusters=k).fit(X)
-    kmeanModel.fit(X)
-    distortions.append(sum(np.min(cdist(X, kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / X.shape[0])
+    km = KMeans(n_clusters=k)
+    km = km.fit(X)
+    #Sum of squared distances of samples to their closest cluster center.
+    Sum_of_squared_distances.append(km.inertia_) 
 
-plt.plot(K, distortions, 'bx-')
+plt.plot(K, Sum_of_squared_distances, 'bx-')
 plt.xlabel('k')
-plt.ylabel('Distortion')
+plt.ylabel('Sum of squared distances')
 plt.title('The Elbow Method showing the optimal k')
 plt.show()
