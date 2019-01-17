25177d3e7e29830a7b5ea43d6ccb3eec86d3741c
Bart van Laatum <hbpvanlaatum123@gmail.com>
2019-01-17 13:02:45 +0100

    'cluster_file'

--- Bart/clusterings.py
+++ Bart/clusterings.py
@@ -1,38 +1,73 @@
 import pandas as pd
 import numpy as np
 from sklearn import cluster as sc
+from kmodes.kmodes import KModes
 import matplotlib.pyplot as plt
 
 def load_csv(file, delim):
     return pd.read_csv(file, delimiter=delim)
 
-def drop_columns(indices):
-    data.drop(data[data.columns[indices]])
+def drop_columns(data, indices):
+    data = data.drop(data[data.columns[indices]], axis=1)
     return data
 
 def cluster(data, clust, n):
     X = data.values
     if clust == 'kmeans':
         clustering = sc.KMeans(n_clusters=n).fit(X)
-    if clust == 'hierarch':
+        labels = clustering.labels_
+    if clust == 'spect':
+        clustering = sc.SpectralClustering(n_clusters=n, assign_labels="discretize", verbose=1,random_state=0).fit(X)
+        labels = clustering.labels_
+    if clust == 'kmodes':
+        labels = KModes(n_clusters=n, init='Huang', n_init=20, verbose=1).fit_predict(X)
 
-    data['labels'] = clustering.labels_
-    print(np.bincount(clustering.labels_))
-    return clustering, data
-    # clustering = sc.cluster.
+    data['label'] = labels
+    print(np.bincount(labels))
+    return data
 
-def plot(data):
+def plot(data, n):
     d = {}
-    for i in range(3):
-        d['label' + i] = data.loc[data.label == 0].astype(float)
-        d['sums' + i] = d['label', ]
+    indices = []
+    y = []
+    for i in range(n):
+        d['label' + str(i)] = data.loc[data.label == i].astype(float)
+        d['sums' + str(i)]  = d['label'+str(i)].sum()
+        for j in range(len(d['sums'+str(i)])):
+            if d['sums'+str(i)][j]/len(d['label' + str(i)]) > .5 and j not in indices:
+                indices.append(j)
+
+    indices.sort()
+    indices = indices[:-1]
+    for i in range(n):
+        sums = d['sums'+str(i)]
+        df = d['label'+str(i)]
+        y.append(sums[df.columns[indices]])
+        print(len(df))
+
+    y = np.array(y)
+    for i in range(len(y)):
+        y[i] = y[i]/len(d['label'+str(i)])
+
+    y = y.T
+    print(y)
+
+    # y[0] = y[0] / len()
+    x = data.columns[indices]
+    df = pd.DataFrame(y,
+                 index=x)
+    plot = df.plot(kind='bar',figsize=(10,3))
+    plt.show()
 
 
 
 if __name__ == '__main__':
+    n = 4
     data = load_csv('../Data/final_data.csv', ',')
-    indices = []
-    data = drop_columns(indices)
-    cluistering, data = cluster(data, 'kmeans', 3)
-    plot(data)
-    # print(data.labels)
+    # print(len(data))
+    indices = [i for i in range(57,167)] + [i for i in range(35,47)] #+ [i for i in range(140,167)]
+    indices.sort()
+    data = drop_columns(data, indices)
+    data = cluster(data, 'spect', n)
+    plot(data, n)
+    # [1040  373  732]
